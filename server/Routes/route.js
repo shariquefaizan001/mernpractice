b80bbc6ea397a99468512efb968a536044fc8443
sharique001 <sharique.immanent@gmail.com>
2022-08-19 18:11:43 +0530

    update

--- server/Routes/route.js
+++ server/Routes/route.js
@@ -3,6 +3,7 @@ const bodyparser = require('body-parser');
 const router = express.Router();
 const mernprac = require("../Model/userschema");
 const { model } = require("mongoose");
+const jwt = require("jsonwebtoken")
 
 
 //   register api
@@ -43,6 +44,57 @@ router.post("/register",  async (req, res) => {
 })
 
 
+
+//   Login  api
+
+router.post("/login",  async (req, res) => {
+    // extract data from request
+    const {  email,  pass} = req.body;
+    //     // console.log(req.body)
+
+    // validate request
+    if (!email  || !pass) {
+        //     // if (email || username || mobilenumber || age || adress)
+        return res.status(404).json(" Please fill  all the data");
+    }
+
+    // if request is valid
+    // try {
+        // check if the user already exists
+        const preuser = await mernprac.findOne({ email: email });
+        const preuserpass = await mernprac.findOne({ pass: pass });
+        console.log(preuser);
+
+
+        if (preuser  && preuserpass) {
+            //  res.status(200).json();
+            const accessToken = jwt.sign({ data: preuser},"jwt_secret_password" )
+            jwt.verify(accessToken, 'jwt_secret_password', (err, authData) => {
+                // const result={accessToken,authData}
+                console.log("jwt.verify");
+                console.log(accessToken);
+
+                if (err) {
+                  console.log(err)
+                  res.sendStatus('403')
+                } else {
+                  res.json({
+                    // message: "post created",
+                    // result
+                    accessToken
+                  });
+                }
+              });
+            
+
+            }else {
+                     res.status(404).json( " login unsuccessful")
+            }
+    
+})
+
+
+
 // getdata routes
 
 router.get("/getdata", async (req, res) => {
@@ -52,6 +104,39 @@ router.get("/getdata", async (req, res) => {
 
     } catch (error) {
         res.status(422).send(error)
+    }
+})
+
+
+
+// get indivisual user data routes
+
+router.get("/singleuserdata/:id", async (req, res) => {
+    try {
+        console.log(req.params)
+        const { id } = (req.params);
+        const singleuser = await mernprac.findById({_id: id});
+        console.log(singleuser);
+       return  res.status(201).json(singleuser);
+    } catch (error) {
+        res.status(422).send(error)
+    }
+})
+
+
+
+// update user data
+router.patch("/userupdate/:id", async (req, res) => {
+    try {
+        console.log(req.params)
+        const { id } = (req.params);
+        console.log(req.body,"bbbbbbbbbbbbbbbbbbbbb")
+        let email = req.body.email
+        const userupdate = await mernprac.findByIdAndUpdate({_id : id},{ email: email})
+        console.log(userupdate,"cccccccccccccccccccccc")
+        res.status(201).json(userupdate)
+    } catch (error) {
+        res.status(422).json(error)
     }
 })
 
